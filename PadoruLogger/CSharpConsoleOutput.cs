407e2ef93c853f3d118e2ddcb4e1c5f14bce4ad2
Maxi <maximuller11@gmail.com>
2020-11-08 17:18:45 -0300

    CSharpConsoleOutput

---
+++ PadoruLogger/CSharpConsoleOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Padoru.Diagnostics
+{
+    class CSharpConsoleOutput : IDebugOutput
+    {
+        public void WriteToOuput(LogType logType, object message, string channel, object context)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
